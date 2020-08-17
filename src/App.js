ba5b16e7945fcc1ad8d71f0ecda193fecaf0d410
Ida Hellqvist <ida.s.hellqvist@gmail.com>
2020-08-17 16:15:31 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+/* class component
+class App extends Reaxt.Component {
+  render() {
+    return <div>Hello world!</div>;
+  }
+}
+*/
+
+// function component
+function App() {
+  return <div>Hello world!</div>;
+}
+
+export default App;
