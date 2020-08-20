418ad800ea51d97f9fdda2ca42119349de4915e2
littlepadawan <ida.s.hellqvist@gmail.com>
2020-08-20 13:05:50 +0200

    Memory game - add leaderboard

--- src/components/memory/index.js
+++ src/components/memory/index.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./index.css";
 import StatusBar from "./StatusBar";
 import MemoryCard from "./MemoryCard";
-import "./index.css";
+import * as utils from "../../utils";
 
 const colors = [
   "pink",
@@ -13,6 +14,7 @@ const colors = [
   "blue",
   "purple",
 ];
+
 function generateCards() {
   const cards = [];
   for (let i = 0; i < colors.length; i++) {
@@ -30,41 +32,108 @@ function generateCards() {
   return cards.sort(() => Math.random() - 0.5);
 }
 
+/* 
+Returns a new array of cards where the specified card (cardToFlip)
+will have a different value of its isFlipped: true changes to false and false to true.
+*/
 function flipCard(cards, cardToFlip) {
   return cards.map((card) => {
     if (card.key === cardToFlip.key) {
-      return {
-        ...card,
-        isFlipped: !card.isFlipped,
-      };
+      return { ...card, isFlipped: !card.isFlipped };
     }
     return card;
   });
 }
 
 function Memory() {
-  // [<current state>, <function to update stare>] = useState(<initial state>) (Gives initial state of the component)
-  // expecting array returned by useState, and you define the first two elements
+  /* Intervals:
+  const startTime = Date.now();
+  const indervalId = setInterval(
+    () => console.log(Date.now() - startTime),
+    1000
+  );
+  clearInterval(indervalId);
+  */
+
+  /*
+  const [cards, setCards] = useState(generateCards());
+  is the same as:
+  const stateArray = useState(generateCards());
+  const cards = stateArray[0];
+  const setCards = stateArray[1];
+  */
+  // [<current state>, <function to update state>] = useState(<initial state>)
   const [game, setGame] = useState({
     cards: generateCards(),
     firstCard: undefined,
     secondCard: undefined,
   });
 
+  const [startTime, setStartTime] = useState(0);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [win, setWin] = useState(false);
+
+  // useState(<effect function>, <dependency array> - optional)
+  // <dependency array>:
+  // * undefined: effect function will be run on every render
+  // * []: effect will run only on the first render
+  // * [value1, value2]: effect will run when any of the values change
+  // effect function returns a cleanup function (optional)
+  //   that runs next time the effect function is run OR when the component
+  //   unmounts (disappears from the DOM)
+  useEffect(() => {
+    if (startTime !== 0 && !win) {
+      const intervalId = setInterval(() => {
+        setElapsedTime(Date.now() - startTime);
+      }, 1000);
+      return () => clearInterval(intervalId);
+    }
+  }, [startTime, win]);
+
+  useEffect(() => {
+    if (win) {
+      utils
+        .saveScore("memory", { name: "Ida", timeMs: elapsedTime })
+        .then(() => console.log("Score saved"))
+        .then(() => utils.fetchLeaderboard("memory"))
+        .then((leaderboard) => console.log(leaderboard));
+    }
+  }, [win]);
+
+  /*
+  Runs every time a card is clicked, flips this card (updates state)
+  */
+  /* (old version)
+  function onCardClicked(clickedCard) {
+    setCards((oldCards) => {
+      return oldCards.map((card) => {
+        if (card.key === clickedCard.key) {
+          return { ...card, isFlipped: !card.isFlipped };
+        }
+        return card;
+      });
+    });
+  }
+  */
+
   function onCardClicked(clickedCard) {
     // If the card is already flipped there is nothing we need to do (write an if-statement with a return; inside)
     if (clickedCard.isFlipped) {
       return;
     }
-    setGame(({ cards, firstCard, secondCard }) => {
+
+    setGame((oldGame) => {
+      const cards = oldGame.cards;
+      const firstCard = oldGame.firstCard;
+      const secondCard = oldGame.secondCard;
+
       // The { cards, firstCard, secondCard } above is the decomposed game object.
       // These three variables represent the previous state, before a card was clicked.
       // We should return the new state, depending on the previous one and on the card that was clicked.
       // There are 4 different cases.
       // 1. If both firstCard and secondCard from the previous state are undefined =>
-      // we should flip the clicked card and set it as the firstCard (create new state and return it)
+      // we should flip the clicked card and set it as the firstCard
       if (!firstCard) {
-        //same as firstCard === undefined
         return {
           cards: flipCard(cards, clickedCard),
           firstCard: clickedCard,
@@ -73,9 +142,14 @@ function Memory() {
       // 2. Else, if firstCard is defined, but secondCard isn't =>
       // we should flip the clicked card, keep the firstCard as is, but set the secondCard
       else if (!secondCard) {
-        //if we have first card but not second card
+        const newCards = flipCard(cards, clickedCard);
+        const isCardFlipped = (card) => card.isFlipped;
+        if (newCards.every(isCardFlipped)) {
+          setWin(true);
+        }
+
         return {
-          cards: flipCard(cards, clickedCard),
+          cards: newCards,
           firstCard: firstCard,
           secondCard: clickedCard,
         };
@@ -87,11 +161,11 @@ function Memory() {
           cards: flipCard(cards, clickedCard),
           firstCard: clickedCard,
         };
-        // 4. Else, if the previous two clicked cards have different colors =>
-        // we should flip the clicked card and flip back firstCard and secondCard,
-        // we should also set the new firstCard and remove secondCard from the state
-      } else {
-        console.log(clickedCard);
+      }
+      // 4. Else, if the previous two clicked cards have different colors =>
+      // we should flip the clicked card and flip back firstCard and secondCard,
+      // we should also set the new firstCard and remove secondCard from the state
+      else {
         let newCards = flipCard(cards, firstCard);
         newCards = flipCard(newCards, secondCard);
         newCards = flipCard(newCards, clickedCard);
@@ -101,20 +175,32 @@ function Memory() {
         };
       }
     });
+
+    setStartTime((oldStartTime) =>
+      oldStartTime === 0 ? Date.now() : oldStartTime
+    );
   }
 
-  // Runs when the restart button is clicked, resets the state with new cards
+  /*
+  Runs when the restart button is clicked, resets the state wth the new cards
+  */
   function onRestart() {
     setGame({
       cards: generateCards(),
       firstCard: undefined,
       secondCard: undefined,
     });
+    setStartTime(0);
+    setElapsedTime(0);
+    setWin(false);
   }
 
   return (
     <div className="game-container">
-      <StatusBar status="Time:" onRestart={onRestart}></StatusBar>
+      <StatusBar
+        status={"Time: " + elapsedTime}
+        onRestart={onRestart}
+      ></StatusBar>
       <div className="memory-grid">
         {game.cards.map((card) => (
           <MemoryCard
@@ -122,7 +208,7 @@ function Memory() {
             color={card.color}
             isFlipped={card.isFlipped}
             onClick={() => onCardClicked(card)}
-          ></MemoryCard>
+          />
         ))}
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  // Your web app's Firebase configuration
+  apiKey: "AIzaSyCscXyE4f3vDr1DhS6dnor00GMCYAuw8vQ",
+  authDomain: "nostalgic-games.firebaseapp.com",
+  databaseURL: "https://nostalgic-games.firebaseio.com",
+  projectId: "nostalgic-games",
+  storageBucket: "nostalgic-games.appspot.com",
+  messagingSenderId: "575235290051",
+  appId: "1:575235290051:web:89b88811f1bbb2d3004376",
+  measurementId: "G-E55MEP3V2K",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(<App />, document.getElementById("app"));
